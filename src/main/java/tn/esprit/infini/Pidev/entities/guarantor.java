314db61ea13b7456de7bd996088481f5b11aca48
Ahmed <75306161+azerty0004@users.noreply.github.com>
2023-02-19 19:19:21 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/infini/Pidev/entities/guarantor.java
@@ -0,0 +1,19 @@
+package tn.esprit.infini.Pidev.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table( name = "guarantor")
+public class guarantor implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idGuarantor")
+    private int idGuarantor ;
+    private String firstNameGuarantor ;
+    private String lastNameGuarantor ;
+    private int cinGuarantor ;
+    private String salary ;
+    private String job ;
+
+}
