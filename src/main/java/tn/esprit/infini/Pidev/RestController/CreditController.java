a27d76158638afe50696756ffee6ca7fc06f9746
azizesprit <aziz.bendriss@esprit.tn>
2023-03-02 16:28:07 +0100

    new service

--- src/main/java/tn/esprit/infini/Pidev/RestController/CreditController.java
+++ src/main/java/tn/esprit/infini/Pidev/RestController/CreditController.java
@@ -1,10 +1,14 @@
 package tn.esprit.infini.Pidev.RestController;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.infini.Pidev.Services.Icreditservice;
 import tn.esprit.infini.Pidev.entities.Credit;
+import tn.esprit.infini.Pidev.entities.Guarantor;
+import tn.esprit.infini.Pidev.entities.Statut;
 
+import java.util.Date;
 import java.util.List;
 @RestController
 @AllArgsConstructor
@@ -36,5 +40,19 @@ public class CreditController {
     {
         icreditservice.deleteCredit(idCredit);
     }
+
+    @GetMapping("/credits")
+    public List<Credit> findBySearchParams(
+            @RequestParam(value = "creditId", required = false) Long creditId,
+            @RequestParam(value = "amount", required = false) Double amount,
+            @RequestParam(value = "date", required = false) Date date,
+            @RequestParam(value = "duration", required = false) Integer duration,
+            @RequestParam(value = "statut", required = false) Statut statut,
+            @RequestParam(value = "guarantor", required = false) Guarantor guarantor
+    ) {
+        return icreditservice.findBySearchParams(creditId, amount, date, duration, statut, guarantor);
+    }
 }
+
+
 
--- src/main/java/tn/esprit/infini/Pidev/Services/Icreditservice.java
+++ src/main/java/tn/esprit/infini/Pidev/Services/Icreditservice.java
@@ -2,25 +2,22 @@ package tn.esprit.infini.Pidev.Services;
 import org.springframework.stereotype.Service;
 import tn.esprit.infini.Pidev.entities.Credit;
 import tn.esprit.infini.Pidev.entities.Statut;
+import tn.esprit.infini.Pidev.entities.Guarantor;
+import tn.esprit.infini.Pidev.entities.TypeCredit;
 
 import java.util.Date;
 import java.util.List;
 @Service
 public interface Icreditservice {
     List<Credit> retrieveAllCredits();
-    List<Credit> findByDateOfObtainingGreaterThan(
-    Date c);
-    List<Credit> findByStatuts(Statut statut);
-
-
-    Credit addCredit(Credit c);
+       Credit addCredit(Credit c);
 
     Credit updateCredit (Credit c);
 
     Credit retrieveCredit (Long id);
 
     void deleteCredit( Long id);
-
+    List<Credit> findBySearchParams(Long creditId, Double amount, Date date, Integer duration, Statut statut, Guarantor guarantor);
 }
 
 
