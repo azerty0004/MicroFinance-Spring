d22d64c291dd648fdc8bf2b4e51cc669b5a9c089
Ahmed <75306161+azerty0004@users.noreply.github.com>
2023-03-03 11:05:21 +0100

    push

--- src/main/java/tn/esprit/infini/Pidev/Services/TransactionService.java
+++ src/main/java/tn/esprit/infini/Pidev/Services/TransactionService.java
@@ -1,10 +1,14 @@
 package tn.esprit.infini.Pidev.Services;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 import tn.esprit.infini.Pidev.Repository.TransactionRepository;
 import tn.esprit.infini.Pidev.entities.Transaction;
 
 import java.util.List;
 
+@Service
+@AllArgsConstructor
 public class TransactionService implements ITransaction {
     TransactionRepository transactionRepository;
     @Override
